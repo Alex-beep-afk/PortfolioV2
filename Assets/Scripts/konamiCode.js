e05254f97e56843bb9553e3beb66060f16e02053
Alexandre_maison <alexandre.prigent2910@gmail.com>
2025-03-02 19:55:23 +0100

    konami code

--- Assets/Scripts/konamiCode.js
+++ Assets/Scripts/konamiCode.js
@@ -1,17 +1,18 @@
 
         // Séquence de touches souhaitée (par exemple, "abc")
-        const sequence = ['s','a','n','t','i'];
+        const sequence = ['ArrowUp','ArrowUp','ArrowDown','ArrowDown','ArrowLeft','ArrowRight','ArrowLeft','ArrowRight','b','a','Enter'];
         let currentSequence = [];
 
         // Écouter les événements de touches
         document.addEventListener('keydown', (event) => {
+            
             // Ajouter la touche pressée à la séquence actuelle
             currentSequence.push(event.key);
 
             // Vérifier si la séquence actuelle correspond à la séquence souhaitée
             if (currentSequence.join('').includes(sequence.join(''))) {
                 // Rediriger l'utilisateur vers une autre page
-                window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+                window.location.href = 'https://alex-beep-afk.github.io/Treasurequest/';
             }
 
             // Limiter la longueur de la séquence actuelle pour éviter les dépassements
